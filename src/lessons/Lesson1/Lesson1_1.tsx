00f47d3c4acef44666c9b14d75988dcfda304c5f
Your NameNatski <143272547+NatskiNa@users.noreply.github.com>
2024-03-28 12:33:22 -0700

    useState practice

--- src/lessons/Lesson1/Lesson1_1.tsx
+++ src/lessons/Lesson1/Lesson1_1.tsx
@@ -1,9 +1,27 @@
+import { ChangeEvent, useState } from 'react';
+
 const Lesson1_1 = () => {
+  const [count, setCount] = useState<number>(0);
+  const [type, setType] = useState('type something here');
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
   return (
     <div>
-      <input type="text" />
-      <button className="border p-2 rounded-md bg-red-100">Add Age</button>
-      <p></p>
+      <input
+        type="text"
+        value={type}
+        onChange={(e: ChangeEvent<HTMLInputElement>) => setType(e.target.value)}
+      />
+      <button
+        onClick={handleClick}
+        className="border p-2 rounded-md bg-red-100"
+      >
+        Click
+      </button>
+      <p>Current count is {count}</p>
     </div>
   );
 };
