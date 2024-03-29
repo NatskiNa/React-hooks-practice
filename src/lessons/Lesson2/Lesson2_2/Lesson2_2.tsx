be66edce58917d4c2f8323b5cee302285dcfbc9b
Your NameNatski <143272547+NatskiNa@users.noreply.github.com>
2024-03-28 17:49:25 -0700

    useState and fetch

--- src/lessons/Lesson2/Lesson2_2/Lesson2_2.tsx
+++ src/lessons/Lesson2/Lesson2_2/Lesson2_2.tsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from 'react';
+import { fetchBio } from './fetchBio';
+
 const Lesson2_2 = () => {
+  const [userName, setUserName] = useState('ShinCode');
+  const [bio, setBio] = useState<string | null>(null);
+
+  useEffect(() => {
+    const startFetching = async () => {
+      const response = await fetchBio(userName);
+      //console.log(response);
+      setBio(response);
+    };
+    startFetching();
+  }, [userName]);
   return (
     <div>
-      <select>
+      <select onChange={(e) => setUserName(e.target.value)} value={userName}>
         <option value="ShinCode">ShinCode</option>
         <option value="TestUser">TestUser</option>
         <option value="SampleUser">SampleUser</option>
@@ -9,7 +23,7 @@ const Lesson2_2 = () => {
 
       <hr />
 
-      <p className="text-black">{"Loading..."}</p>
+      <p className="text-black">{bio ?? 'Loading...'}</p>
     </div>
   );
 };
